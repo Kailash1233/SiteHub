c47189e5250cc24ca38f3f2cf321c9b9fa1ac7c2
Kailash <kailash61203@gmail.com>
2024-07-13 14:17:29 +0530

    fixed env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqGE4KOxKQgy1FZ7cbYpZSAd9KZDNHZxk",
-  authDomain: "sitehub-cb7d6.firebaseapp.com",
-  projectId: "sitehub-cb7d6",
-  storageBucket: "sitehub-cb7d6.appspot.com",
-  messagingSenderId: "390308977120",
-  appId: "1:390308977120:web:9c46923e78cca4d2ebf385"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export {db}
+export { db };
